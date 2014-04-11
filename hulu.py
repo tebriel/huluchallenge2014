70fff804ecad7ea7749c2a777e8615af898ddb16
Chris Moultrie <cmoultrie@endgame.com>
2014-04-11 17:11:49 -0400

    Think I solved it.

---
+++ hulu.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""Written by Chris Moultrie <chris@moultrie.org>
+Available on Github at 
+"""
+
+from collections import defaultdict
+from json import dumps
+from sys import argv
+
+def output(dictionary):
+    """Provided Output Function"""
+    print(dumps(dictionary, sort_keys=True))
+
+def map_sources(source_list, index, sources_map):
+    """Create a hashmap of individual show names to correspond with what lines
+    they occur on in the file in the format:
+    {"ShowTitle":[0,3,77]}
+    Originally tried having sources_map[source] as a set, but .add was 3x slower
+      than append and then casting to a set later
+    """
+    for source in source_list.split(','):
+        sources_map[source].append(index)
+    return sources_map
+
+def process_file(file_name):
+    """Split the file up into sources and blacklists"""
+    # Array of all our lines for retrieval later
+    sources = []
+    # Array of all our blacklists
+    blacklists = []
+    # Indicate that we're done processing the sources
+    sources_done = False
+    # Way to map the sources to their indexes
+    sources_map = defaultdict(list)
+    # What index we're currently on
+    current_index = 0
+    
+    with open(file_name) as groups:
+        for line in groups:
+            line = line.rstrip('\n')
+            if line == "####":
+                sources_done = True
+                continue
+            
+            if sources_done:
+                blacklists.append(line)
+            else:
+                sources.append(line)
+                map_sources(line, current_index, sources_map)
+                current_index += 1
+                
+    return (sources, blacklists, sources_map)
+
+def process_blacklist_items(sources, blacklists, sources_maps):
+    """Figure out which lines have our required blacklist items and print them
+    out"""
+    for blacklist in blacklists:
+        blacklist_sources = blacklist.split(',')
+        to_intersect = []
+        for source in blacklist_sources:
+            if len(sources_map[source]) > 0:
+                to_intersect.append(set(sources_map[source]))
+        counts = defaultdict(int)
+        for intersect in set.intersection(*to_intersect):
+            for source in sources[intersect].split(','):
+                counts[source] += 1
+        for source in blacklist_sources:
+            counts.pop(source, None)
+        output(counts)
+
+
+
+sources, blacklists, sources_map = process_file(argv[1])
+process_blacklist_items(sources, blacklists, sources_map)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
